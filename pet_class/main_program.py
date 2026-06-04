f3f6ce44c4d4dfad08e7bae13d85bc1118f3013d
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-06-04 13:27:21 +0800

    Add setter methods with basic integer type-checking for pet age

--- pet_class/main_program.py
+++ pet_class/main_program.py
@@ -5,4 +5,16 @@ class Pet:
         self.__animal_type = ""
         self.__age = 0
 
-        
+    # --- Setters (Mutators) ---
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        try:
+            self.__age = int(age)
+        except ValueError:
+            print(" Age must be an integer numeric value. Defaulted to 0.")
+            self.__age = 0
